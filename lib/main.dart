74874b4aabe6f4dbcafbd51c64946a0b7ebbd390
Ali Fadlallah <alifadlallah355@gmail.com>
2024-03-19 17:37:04 +0200

    feat: ui for splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:news/ui/splash_screen/splash_screen.dart';
 import 'package:news/utils/customTheme.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
