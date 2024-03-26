2f938701c1dc0a6c251ef465e98c2c8dc3c508c9
Ali Fadlallah <alifadlallah355@gmail.com>
2024-03-26 07:04:17 +0200

    feat: fetch news from api and support the search bar

--- lib/api/api_manager.dart
+++ lib/api/api_manager.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:news/api/api_constants.dart';
 import 'package:news/api/responses/News/NewsResponses.dart';
 import 'package:news/api/responses/Sources/SourcesResponse.dart';
 
 class ApiManager {
-  static const String baseUrl = 'newsapi.org';
 
   static Future<SourcesResponse> getSources(String categoryId) async {
-    Uri uri = Uri.https(baseUrl, 'v2/top-headlines/sources',
-        {'apiKey': apiKey, 'category': categoryId});
+    Uri uri = Uri.https(ApiConstant.baseUrl, 'v2/top-headlines/sources',
+        {'apiKey': ApiConstant.apiKey, 'category': categoryId});
     var response = await http.get(uri);
     var json = jsonDecode(response.body);
     var sourcesResponse = SourcesResponse.fromJson(json);
@@ -17,8 +17,8 @@ class ApiManager {
   }
 
   static Future<NewsResponses> getNews(String sourceID) async {
-    Uri uri = Uri.https(baseUrl, 'v2/everything', {
-      'apiKey': apiKey,
+    Uri uri = Uri.https(ApiConstant.baseUrl, 'v2/everything', {
+      'apiKey': ApiConstant.apiKey,
       'sources': sourceID,
     });
 
@@ -29,8 +29,8 @@ class ApiManager {
   }
 
   static Future<NewsResponses> searchArticle(String keyword) async {
-    Uri uri = Uri.https(baseUrl, 'v2/everything', {
-      'apiKey': apiKey,
+    Uri uri = Uri.https(ApiConstant.baseUrl, 'v2/everything', {
+      'apiKey': ApiConstant.apiKey,
       'q': keyword,
     });
 
